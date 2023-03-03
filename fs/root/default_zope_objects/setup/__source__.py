6f73c287378609051eda268723942fa406857d3c
Jannik B <freezy111111@gmail.com>
2023-03-03 17:27:02 +0000

    add-dockerdb-to-zoperepo (#2)
    Add setup as default object
    
    Cleanup
    
    Adjust StartDocker once more
    
    Add dockerdb as object to zope in case it's missing
    
    Co-authored-by: Klappson <freezy111111@gmail.com>
    Reviewed-on: https://loc.kplays.de:8080/Klappson/Docker-Zope-Setup/pulls/2
    Co-Authored-By: Jannik B <freezy111111@gmail.com>
    Co-Committed-By: Jannik B <freezy111111@gmail.com>

---
+++ fs/root/default_zope_objects/setup/__source__.py
@@ -0,0 +1,5 @@
+print('Hello from the Setup-Script!')
+print('I\'m located at /setup inside Zope and will be called everytime the container starts')
+print('If you can read this, the Zope-Server is ready to answer requests')
+
+return printed
